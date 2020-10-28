c844ac200f52d4754f489a22f5857b4e07e2640d
joeldelabra <joeldelabra@gmail.com>
2020-10-27 21:47:06 -0700

    Visualizer

---
+++ Assets/Scripts/Audiovisualizer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioVisualizer : MonoBehaviour
+{
+    AudioSource aud;
+
+    float[] spectrum;
+
+    float freq;
+
+    Vector3 initScale;
+    [SerializeField]
+    float scaleFactor = 5f;
+
+    void Awake() 
+    {
+        spectrum = new float[256];
+        aud = GetComponent<AudioSource>();    
+    }
+
+    void Start()
+    {
+        initScale = transform.localScale;
+    }    
+
+    void Update()
+    {
+        aud.GetOutputData(spectrum, 0);   
+        for(int i = 0; i < spectrum.Length; i++)
+        {
+            freq = spectrum[i];
+            //Debug.Log(freq);
+        }
+        transform.localScale = (Vector3.one * freq * scaleFactor) + initScale;
+    }
+}
